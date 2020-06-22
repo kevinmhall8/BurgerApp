6961b1ebc7307621e1f03e16efd0b691d5b1156f
Kevin Hall <kevinmhall8@gmail.com>
2020-06-21 18:47:29 -0700

    updating for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = process.env.JAWSDB_URL
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "hall3349",
+    database: "burgers_db",
+  });
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "hall3349",
-  database: "burgers_db",
-});
+}
+
 
 connection.connect(function (err) {
   if (err) {
